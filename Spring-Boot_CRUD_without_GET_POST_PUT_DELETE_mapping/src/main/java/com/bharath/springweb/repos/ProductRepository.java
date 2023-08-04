b2b02c1e328a34b72495c5d3896389a2e0715e60
Sankha Subhra Mondal <sankha123@gmail.com>
2023-08-05 01:07:28 +0530

    Spring-Boot CRUD without GET_POST_PUT_DELETE mapping

--- Spring-Boot_CRUD_without_GET_POST_PUT_DELETE_mapping/src/main/java/com/bharath/springweb/repos/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.bharath.springweb.repos;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.bharath.springweb.entities.Product;
-
-public interface ProductRepository extends JpaRepository<Product, Integer> {
-
-}
